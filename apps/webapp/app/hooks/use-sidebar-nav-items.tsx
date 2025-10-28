a9609e22ae57378cf14c5890f992a54aee4c0c56
nulldev <contact@andybao.me>
2025-10-28 05:46:46 -0400

    Remove asset labels purchase link

--- apps/webapp/app/hooks/use-sidebar-nav-items.tsx
+++ apps/webapp/app/hooks/use-sidebar-nav-items.tsx
@@ -227,13 +227,13 @@ export function useSidebarNavItems() {
   ];
 
   const bottomMenuItems: NavItem[] = [
-    {
-      type: "child",
-      title: "Asset labels",
-      to: `https://store.shelf.nu/?ref=shelf_webapp_sidebar`,
-      Icon: QrCodeIcon,
-      target: "_blank",
-    },
+    //{
+    //  type: "child",
+    //  title: "Asset labels",
+    //  to: `https://store.shelf.nu/?ref=shelf_webapp_sidebar`,
+    //  Icon: QrCodeIcon,
+    //  target: "_blank",
+    //},
     {
       type: "child",
       title: "QR Scanner",
